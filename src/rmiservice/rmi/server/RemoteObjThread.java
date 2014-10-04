80edcebb9109966757d199a982d1b665711ead30
Neil Dhruva <neildhruva@gmail.com>
2014-10-04 17:06:18 -0400

    remote obj thread udpated

--- src/rmiservice/rmi/server/RemoteObjThread.java
+++ src/rmiservice/rmi/server/RemoteObjThread.java
@@ -23,17 +23,15 @@ public class RemoteObjThread implements Runnable{
 
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
-		
 		// get a Class<?>[] for params to be fed into reflection API
-		Class<?>[] params = this.message.argParams.toArray(new Class<?>[this.message.argParams.size()]);
-		
-		
-			try {
+		Class<?>[] params = this.message.argParams.toArray(new Class<?>[this.message.argParams.size()]);		
+		ObjectOutputStream out = null;    
+		try {
 			this.method = object.getClass().getMethod(this.message.methodName, params);
 			 
 			Object result = this.method.invoke(this.object, this.message.args);
 			// write back result to client
+<<<<<<< Updated upstream
 			ObjectOutputStream out = new ObjectOutputStream(this.client.getOutputStream());
 			out.writeObject(result);
 			
@@ -62,6 +60,47 @@ public class RemoteObjThread implements Runnable{
 				e.printStackTrace();
 			}
 			
+=======
+			out = new ObjectOutputStream(this.client.getOutputStream());
+			out.writeObject(result);			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+		    //communication with client error, needs to be handled here.
+			e.printStackTrace();
+			return; //can't let it go to the finally block
+		} catch (SecurityException e) {
+            GenerateRemoteException(e);                
+        } catch (NoSuchMethodException e) {
+            GenerateRemoteException(e);
+        } catch (IllegalArgumentException e) {
+            GenerateRemoteException(e);
+        } catch (IllegalAccessException e) {
+            GenerateRemoteException(e);
+        } catch (InvocationTargetException e) {
+            GenerateRemoteException(e);
+        } finally {
+            try {
+                out.close();
+                client.close();
+            }
+            catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();                
+            }            
+        }
+	}
+	
+	private void GenerateRemoteException(Exception e) {
+        RemoteException rex = new RemoteException();
+        rex.type = e.getClass();
+        rex.message = "RemoteException";
+        try {
+            new ObjectOutputStream(client.getOutputStream()).writeObject(rex);
+        } catch (IOException e1) {
+            // TODO Auto-generated catch block
+            e1.printStackTrace();
+        }
+>>>>>>> Stashed changes
 
 	}
 
